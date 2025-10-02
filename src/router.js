f61c23115a6d51f9449dd4972ea70b3959ab0194
EliLivengood <elivengo@stu.southtexascollege.edu>
2025-10-02 17:15:01 -0500

    updated nav bar --fixed router redirect

--- src/router.js
+++ src/router.js
@@ -24,7 +24,7 @@ const router = createRouter({
 //nav guard
 router.beforeEach((to, _, next) => {
   if (to.meta.requiresAuth && !isAuthenticated.value) {
-    next({name: 'LoginPage', query: {redirect: tofullPath}})
+    next({name: 'LoginPage', query: {redirect: to.fullPath}})
   } else {
     next()
   }
